1153eaf18fa2d6dcb111969d3bc482d310fae5cb
Daniel <daniel@personifyxp.com>
2022-02-28 13:13:05 +0000

    Initial Header Component

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,17 @@
 
 const Header = ({session}) => {
     return ( 
-    <header className="h-20 shadow flex justify-between items-center px-4">
+    <header className="h-20 shadow-sm flex justify-between items-center px-4">
         {/* Hamburger Menu */}
         <div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h7" />
             </svg>
+        </div>
+
+        {/* Search Bar */}
+        <div className="w-8/12 max-w-md">
+            <input type="email" className='w-full p-3 rounded bg-gray-50 outline-none border-2 duration-100 text-sm text-gray-400' placeholder='Search Posts' />
         </div>
 
         {/* User Image */}
